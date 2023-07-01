4fbdcb12a5af2df70bdbcdf8d3c91310dd9ae22c
ThivakarDeivendran <dthivakar1@gmail.com>
2023-07-01 20:56:34 +0530

    first commit for this project

---
+++ src/IPT_JavaTaskProgram/Dupli.java
@@ -0,0 +1,27 @@
+package IPT_JavaTaskProgram;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.graphbuilder.struc.LinkedList;
+
+public class Dupli {
+
+	public static void main(String[] args) {
+
+		String s ="aabbcc";
+		char[] ch = s.toCharArray();
+    for(int i=0;i<ch.length;i++) {
+    	char a = ch[i];
+    	for(int j=0;j<ch.length;j++) {
+    		
+    	}
+    }
+
+		
+
+	}
+
+}
